447295c0e5a194f524e85b6a7081674e2a5ff6f9
RosarioReyes1717 <rr2018-0340@uce.edu.do>
2021-06-29 22:38:26 -0400

    Agregar archivos de proyecto.

---
+++ WebAppRazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppRazorPages.Models;
+
+namespace WebAppRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IDbConnection _connection;
+        private readonly ILogger<IndexModel> _logger;
+        
+        public IndexModel(
+            IDbConnection connection,
+
+            ILogger<IndexModel> logger)
+        {
+            _connection = connection;
+            _logger = logger;
+        }
+
+  
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetProducts()
+        {
+            var products = _connection.Query<product>("select* from Products").AsList();
+            return new OkObjectResult(products);
+        }
+    }
+}
---
+++ WebAppRazorPages/wwwroot/js/index.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    const productsTable = document.querySelector("#productsTable")
+    const productsTableTBody = productsTable.querySelector("tbody")
+
+    axios.get('/Index?handler=products')
+        .then(res => {
+            for (var i in res.data) {
+                const product = res.data[i];
+
+                const tr = document.createElement("tr");
+
+                tr.append(createTd(product.ProductID))
+                tr.append(createTd(product.ProductName))
+                tr.append(createTd(product.SupplierID))
+                tr.append(createTd(product.CategoryID))
+                tr.append(createTd(product.QuantityPerUnit))
+
+                productsTableTBody.appendChild(tr)
+            }
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+    function createTd(content) {
+        const dt = document.createElement('td')
+        dt.textContent = content;
+        return dt
+    }
+
+}) ()
